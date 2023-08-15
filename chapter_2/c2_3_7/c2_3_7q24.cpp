3b714d6b2910a296571a615990bcd9eded51f32a
hleonor <1287607885@qq.com>
2023-08-15 16:25:19 +0800

    2.3.7第23题,时间复杂度O(m)，空间复杂度O(n)，m为表长，n为value取值范围

---
+++ chapter_2/c2_3_7/c2_3_7q24.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Hleonor on 2023-08-15.
+//
+#include "chapter_2_header.h"
+
+// 答案的做法是空间复杂度O(n)
+// 那就没什么稀奇的了，不写了，因为只需要用一个长度为n的数组来记录每个元素是否出现过就行
+// 遍历的过程中需要用到前驱指针进行删除节点的操作
+void keep_one_absolute(ListNode* head)
+{
+    ListNode* cur = head->next;
+    while ()
+}
+
+int main()
+{
+    return 0;
+}

1f5708877b75cf8a713bcc527501a5d8a6ead844
hleonor <1287607885@qq.com>
2023-08-15 20:49:36 +0800

    第三章P67第3题第二小问

---
+++ chapter_3/c3_1_5/c3_1_5q3_2.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Hleonor on 2023-08-15.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    int i_num = 0;
+    bool flag = true;
+    while (cin>> str)
+    {
+        for (char i: str)
+        {
+            if (i == 'i' || i == 'I')
+            {
+                i_num++;
+            }
+            if (i == 'o' || i == 'O')
+            {
+                if (i_num <= 0)
+                {
+                    flag = false;
+                    break;
+                }
+                i_num--;
+            }
+        }
+        if (flag)
+        {
+            cout << "The string is legal." << endl;
+        }
+        else
+        {
+            cout << "The string is illegal." << endl;
+        }
+        flag = true;
+    }
+}

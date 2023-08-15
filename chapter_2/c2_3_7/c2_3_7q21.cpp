f2a786794bfc0f7903fc72b8c8db7d3b4fdecfaa
hleonor <1287607885@qq.com>
2023-08-15 15:46:45 +0800

    2.3.7第21题,时间复杂度O(n)，快慢指针法，由于没有添加额外的域来报名该节点是否被访问过，所以空间复杂度为为O(1)

--- chapter_2/c2_3_7/c2_3_7q21.cpp
+++ chapter_2/c2_3_7/c2_3_7q21.cpp
@@ -7,21 +7,22 @@ using namespace std;
 struct ListNode
 {
     int value;
-    bool visited;
     ListNode* next;
 };
 
 bool hasCircle(ListNode* head)
 {
-    ListNode* cur = head;
-    while (cur)
+    // 快慢指针法，快指针一定可以在有环的情况下追上慢指针
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while (fast && fast->next)
     {
-        if (cur->visited)
+        if (fast == slow)
         {
             return true;
         }
-        cur->visited = true;
-        cur = cur->next;
+        fast = fast->next->next;
+        slow = slow->next;
     }
     return false;
 }
@@ -31,7 +32,6 @@ int main()
     // 构建一个存在环的链表，即最后一个节点的指针指向了环中的某一个节点
     ListNode* head = new ListNode;
     head->value = INT_MIN;
-    head->visited = false;
     head->next = nullptr;
     ListNode* cur = head;
     int n;
@@ -42,13 +42,12 @@ int main()
         cin >> tmp;
         ListNode* node = new ListNode;
         node->value = tmp;
-        node->visited = false;
         node->next = nullptr;
         cur->next = node;
         cur = node;
     }
     // 最后一个节点指向第一个节点
-    cur->next = head->next;
+    /*cur->next = head->next;*/
     string hasCircleStr = hasCircle(head) ? "true" : "false";
     cout << hasCircleStr << endl;
     return 0;

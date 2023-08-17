a1260f9082aad8e1e092201e829708a4c75e247f
hleonor <1287607885@qq.com>
2023-08-17 22:24:15 +0800

    第三章 82页第4题，问题太多，今天先不解决

--- chapter_3/c3_2_6/c3_2_6q4.cpp
+++ chapter_3/c3_2_6/c3_2_6q4.cpp
@@ -15,6 +15,7 @@ class MyQueue
 public:
     QueueNode* front;
     QueueNode* rear;
+    bool book = false;
 
     void push(int x)
     {
@@ -25,7 +26,16 @@ public:
             front->next = front;
             rear = front;
             cout << "Push element successfully, the element is: " << x << endl;
+            book = false;
             return;
+        }
+
+        if (isEmpty())
+        {
+            rear = rear->next;
+            rear->value = x;
+            cout << "Push element successfully, the element is: " << x << endl;
+            front = rear;
         }
 
         if (isFull()) // 此时队满
@@ -37,22 +47,34 @@ public:
             rear->next = node;
             rear = node;
             cout << "Push element successfully, the element is: " << x << endl;
+            book = false;
             return;
         }
         rear = rear->next;
         rear->value = x;
         cout << "Push element successfully, the element is: " << x << endl;
+        book = false;
     }
 
     void pop()
     {
         if (isEmpty())
         {
+            if (book) // 两节点第一次相遇，输出完了以后队空
+            {
+                cout << "Pop successfully, the element is: " << front->value << endl;
+                book = false;
+                return;
+            }
             cout << "Pop failed, the queue is empty." << endl;
             return ;
         }
         cout << "Pop successfully, the element is: " << front->value << endl;
         front = front->next;
+        if (front == rear) // 两个指针第一次重叠
+        {
+            book = true;
+        }
     }
 
     bool isEmpty() const
@@ -88,6 +110,8 @@ int main()
     myQueue.push(3);
     myQueue.pop();
     myQueue.pop();
+    myQueue.pop();
+    myQueue.push(4);
     myQueue.pop();
     myQueue.pop();
     return 0;

e9fd5348d5d63ff55c4412e03a603ec9c6c0429f
hleonor <1287607885@qq.com>
2023-08-18 20:57:56 +0800

    第三章 92页第4题

---
+++ chapter_3/c3_3_7/c_3_3_7q4.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hleonor on 2023-08-18.
+//
+#include <iostream>
+using namespace std;
+
+// 没什么复杂的，类似于一个实时程序，理论上还需要实现并发
+// 简单来说就是设计不同优先级的栈，然后依据条件给出每次上船的序列就行了
+
+int main()
+{
+
+}

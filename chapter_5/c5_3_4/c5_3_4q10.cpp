1c6ef563410a3162c0b71c79d7f290d3db457a6b
hleonor <1287607885@qq.com>
2023-08-25 11:41:12 +0800

    第五章 5.3.4 第10题 找第k个节点

---
+++ chapter_5/c5_3_4/c5_3_4q10.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Mocshere on 2023-08-23.
+//
+#include "chapter5_header.h"
+
+TreeNode* node = nullptr;
+int cnt = 0;
+void find_k(TreeNode* root,int k)
+{
+    if (root)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            node = root;
+            return ;
+        }
+        else
+        {
+            find_k(root->left, k);
+            find_k(root->left, k);
+        }
+    }
+}
+
+int main()
+{
+    TreeNode* root = build_tree();
+    int k = 100;
+    find_k(root, k);
+    if (node)
+    {
+        cout << node->val << endl;
+    }
+    else
+    {
+        cout << "Not found k." << endl;
+    }
+    return 0;
+}
